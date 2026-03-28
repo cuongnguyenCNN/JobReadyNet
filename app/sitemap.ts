93171f2c2b5b111ef02d83a210bf1fa5800f4b69
Nguyễn Ngọc Cương <cuongnguyen71195@gmail.com>
2026-03-28 16:11:35 +0700

    gag

---
+++ app/sitemap.ts
@@ -0,0 +1,29 @@
+import { MetadataRoute } from "next";
+
+async function getPosts() {
+  // sau này fetch từ DB / CMS
+  return [
+    { slug: "dependency-injection-csharp" },
+    { slug: "async-await-csharp" },
+  ];
+}
+
+export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
+  const baseUrl = "https://passdotnet.netlify.app/";
+  const posts = await getPosts();
+
+  return [
+    {
+      url: baseUrl,
+      lastModified: new Date(),
+    },
+    {
+      url: `${baseUrl}/dotnet-interview-questions`,
+      lastModified: new Date(),
+    },
+    ...posts.map((post) => ({
+      url: `${baseUrl}/blog/${post.slug}`,
+      lastModified: new Date(),
+    })),
+  ];
+}
