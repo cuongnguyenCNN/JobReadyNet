2aba935a7d0dba50d6e0c1f7ddc34985ba0e3828
Nguyễn Ngọc Cương <cuongnguyen71195@gmail.com>
2026-03-28 16:04:43 +0700

    gag

--- app/dotnet-interview-questions/layout.tsx
+++ app/dotnet-interview-questions/layout.tsx
@@ -1,5 +1,11 @@
+import { ReactNode } from "react";
+
 export const metadata = {
   title: ".NET Interview Questions (Senior Answers)",
   description:
     "Real .NET interview questions with senior-level answers. Learn how to explain like a senior developer and pass interviews.",
 };
+
+export default function Layout({ children }: { children: ReactNode }) {
+  return <section>{children}</section>;
+}
