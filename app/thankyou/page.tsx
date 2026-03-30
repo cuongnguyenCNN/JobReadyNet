fb8662956d3e7c2fd0b025f4934b25128664531a
Nguyễn Ngọc Cương <cuongnguyen71195@gmail.com>
2026-03-30 13:55:31 +0700

    gag

--- app/thankyou/page.tsx
+++ app/thankyou/page.tsx
@@ -43,7 +43,7 @@ export default function ThankYouPage() {
         </div>
 
         {/* OFFER */}
-        <div className="bg-white border-2 border-black rounded-xl p-6 mb-6 text-center">
+        {/* <div className="bg-white border-2 border-black rounded-xl p-6 mb-6 text-center">
           <h2 className="text-2xl font-bold mb-4">Upgrade to PRO version</h2>
 
           <ul className="text-left mb-4 space-y-2">
@@ -61,8 +61,42 @@ export default function ThankYouPage() {
           <p className="text-red-500 mt-3 font-semibold">
             🔥 Limited price today
           </p>
-        </div>
+        </div> */}
+        <div className="bg-white border-2 border-black rounded-xl p-6 mb-6 text-center">
+          <h2 className="text-2xl font-bold mb-4">
+            Start Practicing Like a Real Interview
+          </h2>
+          <h4 className="text-sm font-bold mb-4">
+            Reading is not enough. Practice is what gets you hired.
+          </h4>
 
+          <ul className="text-left mb-4 space-y-2">
+            <li>✔ Practice real .NET interview questions</li>
+            <li>✔ Get structured answers like a senior dev</li>
+            <li>✔ Improve how you explain (not just memorize)</li>
+          </ul>
+
+          <button
+            onClick={() => {
+              window.gtag?.("event", "click_practice", {
+                step: "thankyou_page",
+              });
+
+              window.location.href = "/";
+            }}
+            className="cursor-pointer bg-black text-white px-6 py-3 rounded-lg text-lg font-semibold hover:opacity-90 transition w-full"
+          >
+            🚀 Start Practice Interview
+          </button>
+
+          <p className="text-xs text-gray-500 mt-2">
+            Free access. No signup required.
+          </p>
+
+          <p className="text-green-600 mt-3 font-semibold">
+            👉 This is how top candidates actually prepare
+          </p>
+        </div>
         {/* BONUS */}
         <div className="bg-white rounded-xl p-6 mb-6 shadow-sm">
           <h3 className="font-semibold mb-2">🎁 Bonus included:</h3>
